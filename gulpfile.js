257ac9e6784303a3654868d58c6eccd3d7bfb130
Lev-Odessit <atarapugin@gmail.com>
2016-09-24 17:06:21 +0300

    stylus was added to app

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp'),
+	stylus = require('gulp-stylus'),
 	wiredep = require('wiredep').stream;
+
+gulp.task('css', function () {
+	return gulp.src('./app/css/styles.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('./app/build/css'));
+});
 
 gulp.task('bower', function () {
 	gulp.src('./app/index.html')
 		.pipe(wiredep({
-			directory: "app/bower_components"
+			directory: "bower_components"
 		}))
 		.pipe(gulp.dest('./app'));
 });
