725841a2e01f089f4e7c0a36091ab85f865e970a
Lev-Odessit <atarapugin@gmail.com>
2016-09-24 22:36:55 +0300

    gulp-rename and gulp-mnify-css was added to dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp'),
 	stylus = require('gulp-stylus'),
+	rename = require('gulp-rename'),
+	minifyCSS = require('gulp-minify-css'),
 	wiredep = require('wiredep').stream;
 
 gulp.task('css', function () {
 	return gulp.src('./app/css/styles.styl')
 		.pipe(stylus())
+		.pipe(gulp.dest('./app/build/css')) // normal main css file
+		.pipe(minifyCSS())
+		.pipe(rename('styles.min.css'))  // minimal main css file
 		.pipe(gulp.dest('./app/build/css'));
 });
 
