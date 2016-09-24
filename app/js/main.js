8ee9cfb14a6569b6eee1d5f977d25e46f4c7c825
Tarapygin Aleksey <tarapygin.aleksey@mailer.ag>
2016-09-24 16:25:26 +0300

    general error and warning in main.js file after eslint tests was fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', function () {
 
 	var registrationApp;
@@ -10,8 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 		setUpListeners: function () {
-			var form = $('form');
-			form.on('submit', registrationApp.submitForm);
+			var form = document.getElemenetById('getForm');
+
+			form.addEventListener('submit', registrationApp.submitForm);
 		},
 
 		submitForm: function (e) {
@@ -35,66 +38,66 @@ document.addEventListener('DOMContentLoaded', function () {
 					alert('Welcome ' + customerInfo.firstName + ' ' + customerInfo.secondName);
 					location.reload();
 				}
-			})
+			});
 		},
 
 		serializeForm: function (form) {
-			if (!form || form.nodeName !== "FORM") {
+			if (!form || form.nodeName !== 'FORM') {
 				return;
 			}
 			var i, j,
 				obj = {};
 			for (i = form.elements.length - 1; i >= 0; i = i - 1) {
-				if (form.elements[i].name === "") {
+				if (form.elements[i].name === '') {
 					continue;
 				}
 				switch (form.elements[i].nodeName) {
-					case 'INPUT':
-						switch (form.elements[i].type) {
-							case 'text':
-							case 'hidden':
-							case 'password':
-							case 'button':
-							case 'reset':
-							case 'submit':
-								obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
-								break;
-							case 'checkbox':
-							case 'radio':
-								if (form.elements[i].checked) {
-									obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
-								}
-								break;
-							case 'file':
-								break;
-						}
-						break;
-					case 'TEXTAREA':
+				case 'INPUT':
+					switch (form.elements[i].type) {
+					case 'text':
+					case 'hidden':
+					case 'password':
+					case 'button':
+					case 'reset':
+					case 'submit':
 						obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
 						break;
-					case 'SELECT':
-						switch (form.elements[i].type) {
-							case 'select-one':
-								obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
-								break;
-							case 'select-multiple':
-								for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
-									if (form.elements[i].options[j].selected) {
-										obj[form.elements[i].name] = decodeURIComponent(form.elements[i].options[j].value);
-									}
-								}
-								break;
+					case 'checkbox':
+					case 'radio':
+						if (form.elements[i].checked) {
+							obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
 						}
 						break;
-					case 'BUTTON':
-						switch (form.elements[i].type) {
-							case 'reset':
-							case 'submit':
-							case 'button':
-								obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
-								break;
+					case 'file':
+						break;
+					}
+					break;
+				case 'TEXTAREA':
+					obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
+					break;
+				case 'SELECT':
+					switch (form.elements[i].type) {
+					case 'select-one':
+						obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
+						break;
+					case 'select-multiple':
+						for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
+							if (form.elements[i].options[j].selected) {
+								obj[form.elements[i].name] = decodeURIComponent(form.elements[i].options[j].value);
+							}
 						}
 						break;
+					}
+					break;
+				case 'BUTTON':
+					switch (form.elements[i].type) {
+					case 'reset':
+					case 'submit':
+					case 'button':
+						obj[form.elements[i].name] = decodeURIComponent(form.elements[i].value);
+						break;
+					}
+					break;
 				}
 			}
 			return obj;
@@ -102,10 +105,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		validateForm: function () {
 
-			var password = $('#customerPassword').val(),
-				confirmPassword = $('#customerRepeatPassword').val();
+			var passwordValue = document.getElementById('customerPassword').value,
+				confirmPasswordValue = document.getElementById('customerRepeatPassword').value;
 
-			return password === confirmPassword;
+			return passwordValue === confirmPasswordValue;
 
 		}
 
